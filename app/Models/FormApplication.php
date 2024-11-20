8449d4c2e3af044883c9b444aa4bdb9af71bdc88
FachriPahlevi <nurfachrialisnan@gmail.com>
2024-11-20 10:49:13 +0700

    login

---
+++ app/Models/FormApplication.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormApplication extends Model
+{
+    use HasFactory;
+
+    protected $table = 'forms';
+    protected $guarded = [];
+}
---
+++ resources/js/Layouts/GuestLayout.jsx
@@ -0,0 +1,22 @@
+import ApplicationLogo from '@/Components/ApplicationLogo';
+import { Link } from '@inertiajs/react';
+
+export default function Guest({ children }) {
+    return (
+        <div className="min-h-screen grid grid-cols-2 bg-white h-screen">
+            <div className='items-center justify-center flex'>
+                <div className="w-full border-gray-200 border sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                    {children}
+                </div>
+            </div>
+            <div className='cols-span-2 h-screen bg-blue-900 flex justify-center items-center text-center my-auto font-bold'>
+                <div className='text-white'>
+                    <h1 className='font-bold text-7xl mb-8'> Selamat Datang</h1>
+                    <h1 className='font-light text-3xl'>Standar Operasional Perusahaan (SOP) </h1>
+                    <h1 className='font-light text-3xl'>Sinergi Mitra Investama (SMI)</h1>
+                </div>
+            </div>
+            
+        </div>
+    );
+}
